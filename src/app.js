e170e455e38e88f49a7956fa88662d91095d302f
jsosa26 <jesus.asm26@gmail.com>
2022-12-28 02:43:36 +0000

    primeros pasos de js y arreglos de css

--- src/app.js
+++ src/app.js
@@ -7,5 +7,9 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domains = getAllCombinations(pronoun, adj, noun, extension);
+  let output = document.getElementById("dominios");
+  output.innerHTML = domains.join("<br>");
 };
+
+function getAllCombinations(lista_1, lista_2, lista_3, lista_4) {}
